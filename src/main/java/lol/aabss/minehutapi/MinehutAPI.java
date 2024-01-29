ed166f27bb936691d01aa08ee385fe9a1f3739ca
aabssmc <lilmikeyninja203@gmail.com>
2024-01-28 21:30:23 -0800

    made it update

--- src/main/java/lol/aabss/minehutapi/MinehutAPI.java
+++ src/main/java/lol/aabss/minehutapi/MinehutAPI.java
@@ -17,8 +17,6 @@ public class MinehutAPI {
      * The api url.
      */
     private static final String url = "https://api.minehut.com/";
-    public static final JSONObject simple = new JSONObject(request(url, "network/simple_stats"));
-    public static final JSONObject homepage = new JSONObject(request(url, "network/homepage_stats"));
 
     /**
      * @param endpoint The api endpoint (ex: servers)
@@ -45,34 +43,39 @@ public class MinehutAPI {
      * @return Gets the online server count.
      */
     public static int getOnlineServerCount(){
-        return simple.getInt("server_count");
+        String servers = request(url, "network/simple_stats");
+        return new JSONObject(servers).getInt("server_count");
     }
 
     /**
      * @return Gets the ram count.
      */
     public static int getRamCount(){
-        return simple.getInt("ram_count");
+        String servers = request(url, "network/simple_stats");
+        return new JSONObject(servers).getInt("ram_count");
     }
 
     /**
      * @return Gets the max amount of the ram.
      */
     public static int getRamMax(){
-        return simple.getInt("ram_max");
+        String servers = request(url, "network/simple_stats");
+        return new JSONObject(servers).getInt("ram_max");
     }
 
     /**
      * @return Gets the amount of all servers registered.
      */
     public static int getServerCount(){
-        return homepage.getInt("server_count");
+        String servers = request(url, "network/homepage_stats");
+        return new JSONObject(servers).getInt("server_count");
     }
 
     /**
      * @return Gets the amount of all users registered.
      */
     public static int getUserCount(){
-        return homepage.getInt("user_count");
+        String servers = request(url, "network/homepage_stats");
+        return new JSONObject(servers).getInt("user_count");
     }
 }
--- src/main/java/lol/aabss/minehutapi/players/Players.java
+++ src/main/java/lol/aabss/minehutapi/players/Players.java
@@ -3,59 +3,64 @@ package lol.aabss.minehutapi.players;
 import org.json.JSONObject;
 
 import static lol.aabss.minehutapi.MinehutAPI.request;
-import static lol.aabss.minehutapi.MinehutAPI.simple;
 
 public class Players {
 
     private static final String url = "https://api.minehut.com/";
-    public static final JSONObject distribution = new JSONObject(request(url, "network/players/distribution"));
 
     /**
      * @return Gets the total amount of players across the whole network.
      */
     public static int getOnlinePlayerCount(){
-        return simple.getInt("player_count");
+        String servers = request(url, "network/simple_stats");
+        return new JSONObject(servers).getInt("player_count");
     }
 
     /**
      * @return Gets the total amount of bedrock players across the whole network.
      */
     public static int getBedrockTotal(){
-        return distribution.getInt("bedrockTotal");
+        String servers = request(url, "network/players/distribution");
+        return new JSONObject(servers).getInt("bedrockTotal");
     }
 
     /**
      * @return Gets the total amount of java players across the whole network.
      */
     public static int getJavaTotal(){
-        return distribution.getInt("javaTotal");
+        String servers = request(url, "network/players/distribution");
+        return new JSONObject(servers).getInt("javaTotal");
     }
 
     /**
      * @return Gets the total amount of bedrock players in the lobby.
      */
     public static int getBedrockLobby(){
-        return distribution.getInt("bedrockLobby");
+        String servers = request(url, "network/players/distribution");
+        return new JSONObject(servers).getInt("bedrockLobby");
     }
 
     /**
      * @return Gets the total amount of bedrock players in servers.
      */
     public static int getBedrockPlayerServer(){
-        return distribution.getInt("bedrockPlayerServer");
+        String servers = request(url, "network/players/distribution");
+        return new JSONObject(servers).getInt("bedrockPlayerServer");
     }
 
     /**
      * @return Gets the total amount of java players in the lobby.
      */
     public static int getJavaLobby(){
-        return distribution.getInt("javaLobby");
+        String servers = request(url, "network/players/distribution");
+        return new JSONObject(servers).getInt("javaLobby");
     }
 
     /**
      * @return Gets the total amount of java players in servers.
      */
     public static int getJavaPlayerServer(){
-        return distribution.getInt("javaPlayerServer");
+        String servers = request(url, "network/players/distribution");
+        return new JSONObject(servers).getInt("javaPlayerServer");
     }
 }
